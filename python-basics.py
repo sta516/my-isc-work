3f122587109030123ebec93af62a79198ff69a8b
user01 <user01@see-l11240.leeds.ac.uk>
2018-11-20 15:10:32 +0000

    Python exercises

---
+++ python-basics.py
@@ -0,0 +1,18 @@
+#Python basics course
+
+#1:
+course = 'python'
+rating = 5
+print(course)
+print(rating)
+print(course * rating)
+
+#2:
+b = 6
+d = 3
+a = ((b ** 2) + (d ** 2)) * 0.5
+print(a)
+
+#3:
+a = int(a)
+print(type(a))
